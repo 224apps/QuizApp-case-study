74ebe2b9cbc868be39cfefe09c82ec7157ae4564
224apps <224apps@gmail.com>
2022-07-05 18:10:55 -0400

    test start and answer first and second questions, given the flow with three questions, router route to second and third questions

--- QuizEngine/QuizEngineTests/FlowTest.swift
+++ QuizEngine/QuizEngineTests/FlowTest.swift
@@ -47,7 +47,16 @@ class FlowTest: XCTestCase {
         XCTAssertEqual(router.routedQuestions, ["Q1","Q2"])
     }
     
-
+    func  test_startAndAnswerFirstAndSecondQuestions_withThreeQuestions_routeToSecondAndThridQuestion(){
+        let sut = makeSUT(questions: ["Q1","Q2", "Q3"])
+        sut.start()
+        
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+      
+        XCTAssertEqual(router.routedQuestions, ["Q1","Q2", "Q3"])
+    }
+    
     //MARK: - Helpers
     
     func makeSUT(questions: [String]) -> Flow {
