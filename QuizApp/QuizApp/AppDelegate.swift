030cee9a56f5241ad07be06023becd5817452c0a
224apps <224apps@gmail.com>
2022-07-07 17:13:38 -0400

    Create QuizApp  iOS Project

---
+++ QuizApp/QuizApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  QuizApp
+//
+//  Created by Abdoulaye Diallo on 7/7/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
