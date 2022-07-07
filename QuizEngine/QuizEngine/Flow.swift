28589f64585c54de250d76551f5fc6b5f08c1610
224apps <224apps@gmail.com>
2022-07-07 10:00:22 -0400

    start and answer first and second questions, given the flow has two questions, router route to result

--- QuizEngine/QuizEngine/Flow.swift
+++ QuizEngine/QuizEngine/Flow.swift
@@ -16,6 +16,7 @@ protocol Router {
 class Flow{
     private let router: Router
     private let questions: [String]
+    private var result: [String: String] = [:]
     
     init(questions: [String], router: Router) {
         self.questions = questions
@@ -31,14 +32,15 @@ class Flow{
     }
     
     private func routeNext(from question: String) -> Router.AnswerCallback {
-        return { [weak self ] _ in
+        return { [weak self ] answer in
             guard let self = self else { return }
             if let currentQuestionIndex = self.questions.firstIndex(of: question) {
+                self.result[question] = answer
                 if currentQuestionIndex+1 < self.questions.count {
                     let nextQuestion = self.questions[currentQuestionIndex+1]
                     self.router.routeTo(question: nextQuestion, answerCallback: self.routeNext(from: nextQuestion))
                 }else {
-                    self.router.routeTo(result: ["Q1": "A1"])
+                    self.router.routeTo(result: self.result)
                 }
             }
         }
