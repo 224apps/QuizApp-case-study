70732f3eba122e6f0caf74fcd64fc9bdfc22bc46
224apps <224apps@gmail.com>
2022-07-07 20:09:09 -0400

    create a  `makeSUT` factory method to clean 	`QuestionViewController` tests

--- QuizApp/QuizAppTests/QuestionViewControllerTests.swift
+++ QuizApp/QuizAppTests/QuestionViewControllerTests.swift
@@ -11,34 +11,31 @@ import XCTest
 class QuestionViewControllerTests: XCTestCase {
 
     func test_viewDidLoad_rendersQuestionHeaderText(){
-        let sut = QuestionViewController(question: "Q1", options: [])
-        
-        _ = sut.view
-        
-        XCTAssertEqual(sut.headerLabel.text, "Q1")
+        XCTAssertEqual(makeSUT(question: "Q1").headerLabel.text, "Q1")
     }
     
     func test_viewDidLoad_withZeroOptions_rendersZeroOptions(){
-        let sut = QuestionViewController(question: "Q1", options:[])
-        _ = sut.view
-        
-        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
+        XCTAssertEqual(makeSUT(options: []).tableView.numberOfRows(inSection: 0), 0)
     }
     
     func test_viewDidLoad_withOneOptions_rendersOneOptions(){
-        let sut = QuestionViewController(question: "Q1", options:["A1"])
-        _ = sut.view
-        
-        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
+        XCTAssertEqual(makeSUT(options:["A1"]).tableView.numberOfRows(inSection: 0), 1)
     }
     
     func test_viewDidLoad_withOneOption_rendersOneOptionText(){
-        let sut = QuestionViewController(question: "Q1", options:["A1"])
-        _ = sut.view
-        
+        let sut = makeSUT(options:["A1"])
+      
         let indexPath = IndexPath(row: 0, section: 0)
         let cell = sut.tableView.dataSource?.tableView(sut.tableView, cellForRowAt: indexPath)
         
         XCTAssertEqual(cell?.textLabel?.text, "A1")
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(question:String = "", options: [String] = []) -> QuestionViewController {
+        let sut = QuestionViewController(question: question, options:options)
+        _ = sut.view
+        return sut
     }
 }
