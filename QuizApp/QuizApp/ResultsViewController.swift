0d4cde77b9600c646ba3efd90fba7b5850cf8d7a
224apps <224apps@gmail.com>
2022-07-20 12:53:54 -0400

    test view did load renders answers

--- QuizApp/QuizApp/ResultsViewController.swift
+++ QuizApp/QuizApp/ResultsViewController.swift
@@ -10,15 +10,28 @@ import UIKit
 class ResultsViewController: UIViewController {
     
     @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     private var summary: String = ""
+    private var answers = [String]()
     
-    convenience init(summary: String){
+    convenience init(summary: String, answers: [String]){
         self.init()
         self.summary = summary
+        self.answers = answers
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         headerLabel.text = summary
+    }
+}
+
+extension ResultsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return answers.count
     }
 }
--- QuizApp/QuizAppTests/ResultsViewControllerTest.swift
+++ QuizApp/QuizAppTests/ResultsViewControllerTest.swift
@@ -11,10 +11,19 @@ import XCTest
 class ResultsViewControllerTest: XCTestCase {
 
     func test_viewDidLoad_rendersSummary(){
-        let sut = ResultsViewController(summary:"a summary")
-        
-        _ = sut.view
-   
-        XCTAssertEqual(sut.headerLabel.text, "a summary")
+        XCTAssertEqual(makeSUT(summary:"a summary").headerLabel.text, "a summary")
     }
+    
+    func test_viewDidLoad_renderAnswers(){
+        XCTAssertEqual(makeSUT(answers: []).tableView.numberOfRows(inSection: 0), 0)
+        XCTAssertEqual(makeSUT(answers: ["A1"]).tableView.numberOfRows(inSection: 0), 1)
+    }
+    
+    //MARK: - Helpers
+    private func makeSUT(summary: String = "", answers: [String] = []) -> ResultsViewController {
+        let sut = ResultsViewController(summary: summary, answers: answers)
+        _ = sut.view
+        return sut
+    }
+
 }
